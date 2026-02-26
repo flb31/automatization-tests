c2d73ea3378af25c958b7effcdc08507e21219ba
Fabian Luna <flb031@gmail.com>
2026-02-26 09:20:07 -0500

    add coverage task

--- src/test-setup/index.ts
+++ src/test-setup/index.ts
@@ -2,6 +2,6 @@ import TestSuiteError from "./test-suite-error";
 import { sizesTest, env } from "./constants";
 
 const sizeValue = Object.values(sizesTest);
-if (!sizeValue.includes(env.size)) {
+if (env.size !== "all-sizes" && !sizeValue.includes(env.size)) {
   throw new TestSuiteError(`${env.size} is not a size available. Only "${sizeValue}"`);
 }
